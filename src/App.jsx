be3767a345f8cb139012e7556b62aea41ad050c2
Biswajeet Barik <biswajeetbarik3936@gmail.com>
2025-01-02 18:26:34 +0530

    made changes in app component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 import ToDoList from "./components/ToDoList";
 function App() {
   return (
-    <div className="home flex flex-col w-full min-h-screen gap-8 bg-[#122023]  text-white ">
+    <div className="home flex flex-col w-full min-h-screen pb-12 gap-8 bg-[#122023]  text-white ">
       <Header />
       <ToDoList />
     </div>
